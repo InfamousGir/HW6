6a14f27ebae12e75bae2dc59ddb57967aa452496
John Batestingq <44099255+BAEtesting@users.noreply.github.com>
2018-12-07 23:07:41 -0800

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package homework5;
+package homework6;
 
 import homework5.Card;
 import homework5.DestinationCard;
